e02118b1482b289a1edbd5bce71bfb96b7c6ade2
Solowise130 <iniodusolomon@gmail.com.com>
2023-03-12 20:03:49 +0100

    Updated

--- PHP-WORK_SPACE/proj.php
+++ PHP-WORK_SPACE/proj.php
@@ -4,12 +4,11 @@ $question = "How fun is PHP?";
 echo "$text \n";
 echo "$question \n";
 
-//To declare and ouput the variable differently, see below.
+//To declare an ouput the variable differently, see below.
 
 $text = "PHP is fun";
 $question = "How is PHP fun?";
 
 echo " Well, the Programming Language $text \n";
 echo "Can you answer the question, $question";
-
 ?>
